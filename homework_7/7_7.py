7339a6a8905395016a3b6c15aebf1a76008c9f83
Nyuta14 <anutakronivez@gmail.com>
2023-02-04 20:29:59 +0200

    Homework_7

---
+++ homework_7/7_7.py
@@ -0,0 +1,13 @@
+class MyIt:
+
+   def __init__(self, data):
+       self.data = data
+
+   def __iter__(self):
+       for item in self.data:
+           yield item
+
+iterable = MyIt([1, 2, 3, 4, 5])
+
+for item in iterable:
+   print(item)
