fd6856c26e88a6ad43ec33234f1008c8ef878274
Nyuta14 <anutakronivez@gmail.com>
2023-02-10 11:56:36 +0200

    Homework_8

---
+++ homework_8/8_8.py
@@ -0,0 +1,28 @@
+import time
+
+def time_func(function):
+    def counter(*args, **kwargs):
+        start_time = time.time()
+        result = function(*args, **kwargs)
+        end_time = time.time()
+        print(f'Time taken to execute {function.__name__}: {end_time - start_time} seconds')
+        return result
+    return counter
+
+@time_func
+def some_func():
+    time.sleep(3)
+    return
+
+some_func()
+
+"""
+
+>>> assertion
+result
+
+"""
+
+
+
+
